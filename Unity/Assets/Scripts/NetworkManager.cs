153bb53a9a5b2d14e8d649401b21866e36a4719e
joshua-wellings <joshssss@hotmail.co.uk>
2016-11-02 23:53:32 +0000

    adding a player class to work with multiplayer

--- Unity/Assets/Scripts/NetworkManager.cs
+++ Unity/Assets/Scripts/NetworkManager.cs
@@ -4,52 +4,87 @@ using System.Collections;
 // Code sourced from multiplayer tutorial.
 // multiplayer tutorial: http://www.paladinstudios.com/2013/07/10/how-to-create-an-online-multiplayer-game-with-unity/
 
-public class NetworkManager : MonoBehaviour {
+public class NetworkManager : MonoBehaviour
+{
+	private const string typeName = "UniqueGameName";
+	private const string gameName = "RoomName";
 
-	private const string typeName = "SpaceRace";
-	private const string gameName = "level1";
+	private bool isRefreshingHostList = false;
 	private HostData[] hostList;
 
-	private void StartServer () {
-		Network.InitializeServer (8, 10000, !Network.HavePublicAddress ());
-		MasterServer.RegisterHost (typeName, gameName);
-	}
+	public GameObject playerPrefab;
 
-	void onServerInitialised () {
-		Debug.Log ("Server has initialised");
-	}
+	// adding the buttons to the GUI
+	void OnGUI() 
+	{
+		if (!Network.isClient && !Network.isServer)
+		{
+			if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+				StartServer();
 
+			if (GUI.Button(new Rect(100, 250, 250, 100), "Refresh Hosts"))
+				RefreshHostList();
 
-
-	private void RefreshHostList() {
-		MasterServer.RequestHostList (typeName);
-	}
-
-	void OnMasterServerEvent (MasterServerEvent msEvent) {
-		if (msEvent == MasterServerEvent.HostListReceived)
-			hostList = MasterServer.PollHostList ();
-	}
-
-	private void JoinServer (HostData hostData) {
-		Network.Connect (hostData);
-	}
-
-	void onConnectedToServer() {
-		Debug.Log ("Server has been joined");
-	}
-
-	void onGUI() {
-		if (!Network.isClient && !Network.isServer) {
-			if (GUI.Button (new Rect (100, 100, 250, 100), "Create a server"))
-				StartServer ();
-			if (GUI.Button (new Rect (100, 250, 250, 100), "Refresh hosts"))
-				RefreshHostList ();
-			if (hostList != null) {
-				for (int i = 0; i < hostList.Length; i++) {
-					if (GUI.Button (new Rect (400, 100 + (100 * i), 300, 100), hostList [i].gameName))
-						JoinServer (hostList [i]);
+			if (hostList != null)
+			{
+				for (int i = 0; i < hostList.Length; i++)
+				{
+					if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList[i].gameName))
+						JoinServer(hostList[i]);
 				}
 			}
 		}
+	}
+
+	//Starting server with 5 players (max), on a port of 25000, and checking if the machine has a public address
+	// the server is then registered to the master server, a server held by unity.
+	private void StartServer()
+	{
+		Network.InitializeServer(5, 25000, !Network.HavePublicAddress());
+		MasterServer.RegisterHost(typeName, gameName);
+	}
+
+	// when a server is initialized, a player is automatically spawned.
+	void OnServerInitialized()
+	{
+		SpawnPlayer();
+	}
+
+	// updating the host list
+	void Update()
+	{
+		if (isRefreshingHostList && MasterServer.PollHostList().Length > 0)
+		{
+			isRefreshingHostList = false;
+			hostList = MasterServer.PollHostList();
+		}
+	}
+
+	//requesting the host list
+	private void RefreshHostList()
+	{
+		if (!isRefreshingHostList)
+		{
+			isRefreshingHostList = true;
+			MasterServer.RequestHostList(typeName);
+		}
+	}
+
+	// code to join the server once the server is available.
+	private void JoinServer(HostData hostData)
+	{
+		Network.Connect(hostData);
+	}
+
+	// if a server is joined, a player is spawned using the SpawnPlayer method.
+	void OnConnectedToServer()
+	{
+		SpawnPlayer();
+	}
+
+	// method used to instantiate a player.
+	private void SpawnPlayer()
+	{
+		Network.Instantiate(playerPrefab, Vector3.up * 5, Quaternion.identity, 0);
 	}
 }
