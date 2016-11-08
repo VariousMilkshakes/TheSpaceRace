a23eca857155676ae7c03ec9dfd08439c7dd5d7d
VariousMilkshakes <vm@hadesoft.io>
2016-11-08 11:59:27 +0000

    tracker hack start

--- Unity/Assets/Scripts/Utils/UiHack.cs
+++ Unity/Assets/Scripts/Utils/UiHack.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System;
 
 using SpaceRace;
+using SpaceRace.PlayerTools;
+
 using UnityEngine.UI;
 
 namespace SpaceRace.Utils
@@ -15,17 +17,30 @@ namespace SpaceRace.Utils
 
 		private List<GameObject> activeUIItems;
 
+		public Player currentPlayer;
+
+		#region ResourceTrackers
+		public GameObject WoodTracker;
+		public GameObject PopTracker;
+		public GameObject MoneyTracker;
+		#endregion
+
 
 		// Use this for initialization
 		void Start()
 		{
 			activeUIItems = new List<GameObject>();
+			//Text currentText = WoodTracker.GetComponent<Text>();
+			//currentText.text = "" + currentPlayer.Inventory.CheckResource(PlayerTools.Resources.Wood);
+			Text currentText = MoneyTracker.GetComponent<Text>();
+			currentText.text = "0";
+			currentText = PopTracker.GetComponent<Text>();
+			currentText.text = "10";
 		}
 
 		// Update is called once per frame
 		void Update()
 		{
-
 		}
 
 		public void DisplayBuildings (Tile targetTile)
@@ -52,7 +67,7 @@ namespace SpaceRace.Utils
 				{
 					Config buildingConfigs = GameRules.CONFIG_REPO["Buildings"];
 					string spritePath = buildingConfigs.LookForProperty(building.Name, "Sprite.All").Value;
-					Texture2D rawSprite = Resources.Load(spritePath, typeof(Texture2D)) as Texture2D;
+					Texture2D rawSprite = UnityEngine.Resources.Load(spritePath, typeof(Texture2D)) as Texture2D;
 					menuButton.image.sprite = Sprite.Create(rawSprite,
 						new Rect(0, 0, buttonSize, buttonSize),
 						new Vector2());
