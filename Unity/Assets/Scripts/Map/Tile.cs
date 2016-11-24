21b88ce969259144a4b9801809a2736144e1a34f
Will <wll.hrdwk@gmail.com>
2016-11-24 14:56:46 +0000

    meta files

--- Unity/Assets/Scripts/Map/Tile.cs
+++ Unity/Assets/Scripts/Map/Tile.cs
@@ -214,6 +214,7 @@ public class Tile: MonoBehaviour{
 		builder.TrackBuilding(newBuilding);
 		building = newBuilding;
 		sr.sprite = building.ActiveSprite;
+		sr.sortingOrder = 1;
 		builder.Inventory.AddResource(building.OnBuild());
 	}
 
