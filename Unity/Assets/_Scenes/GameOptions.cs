df472dd91316842aa8f651bbc97f8fd877a11ced
deepamgt <deepamangrati@gmail.com>
2016-12-01 16:42:29 +0000

    Changes to option
    added multiplayer buttons

---
+++ Unity/Assets/_Scenes/GameOptions.cs
@@ -0,0 +1,19 @@
+﻿
+public class GameOptions 
+{  //class that is blueprint of the options
+    public int mapHeight;
+    public int mapWidth;
+    public string worldSeed;
+    public double waterCoverage;
+    
+
+   // public MapGenerator mapGenerator;
+    //public GameOptions gameOptions;
+
+
+
+
+
+
+
+}
---
+++ Unity/Assets/_Scenes/OptionManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class OptionManager : MonoBehaviour {
+    public InputField mapHeightInputField;
+    public InputField mapWidthtInputField;
+    public InputField worldSeedInputField;
+    public Slider waterCoverageSlider;
+    
+
+    //public Resolution resolutions;
+    public MapGenerator mapGenerator;
+    public GameOptions gameOptions;
+   
+
+    void OnEnable()
+    {
+        gameOptions = new GameOptions();
+
+
+        mapHeightInputField.onValueChanged.AddListener(delegate { OnMapHeightChange(); });
+        mapWidthtInputField.onValueChanged.AddListener(delegate { OnMapWidthChange(); });
+        worldSeedInputField.onValueChanged.AddListener(delegate { OnWorldSeedChange(); });
+        waterCoverageSlider.onValueChanged.AddListener(delegate { OnWaterCoverageChange(); });
+        
+
+        //  mapGenerator = Screen.;
+
+    }
+
+    //GameOptions.SetMapHeight(int height) { 
+    //  MapGenerator.SetMapWidth (String 
+
+
+
+    public void OnMapHeightChange()
+    {
+     //MapGenerator.setMap
+    }
+    public void OnMapWidthChange()
+    {
+        
+    }
+    public void OnWorldSeedChange()
+    {
+      
+    }
+    public void OnWaterCoverageChange()
+    {
+
+    }
+   
+
+}
+//going to handle the logic/the UI of the options
