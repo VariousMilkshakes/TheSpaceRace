d996881467f7d65382add957a7070e761d1c3a4b
Clare Buckley <bucklec2@aston.ac.uk>
2017-02-05 17:14:30 +0000

    Remove un-needed imports from TownHall.cs

--- Unity/Assets/Scripts/World/Buildings/Collection/TownHall.cs
+++ Unity/Assets/Scripts/World/Buildings/Collection/TownHall.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using SpaceRace.World.Buildings;
 using SpaceRace.PlayerTools;
 using UnityEngine;
-using SpaceRace.Utils;
 using System.Collections.Generic;
-
 using System.Linq;
 
 
