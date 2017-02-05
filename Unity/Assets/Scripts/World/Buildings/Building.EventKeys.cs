fb5780d563f3d4a0f78169b8de1844f1a93bfd70
Clare Buckley <bucklec2@aston.ac.uk>
2017-02-05 22:38:15 +0000

    Remove un-used imports in Building.EventKeys.cs

--- Unity/Assets/Scripts/World/Buildings/Building.EventKeys.cs
+++ Unity/Assets/Scripts/World/Buildings/Building.EventKeys.cs
@@ -1,12 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using SpaceRace.PlayerTools;
-using SpaceRace.Utils;
-
-using UnityEngine;
 
 namespace SpaceRace.World.Buildings
 {
