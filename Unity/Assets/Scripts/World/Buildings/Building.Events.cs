05d93d850740c5241248d2067a2066dff5ed5356
Clare Buckley <bucklec2@aston.ac.uk>
2017-02-05 22:40:11 +0000

    Remove un-used imports from Building.Events.cs

--- Unity/Assets/Scripts/World/Buildings/Building.Events.cs
+++ Unity/Assets/Scripts/World/Buildings/Building.Events.cs
@@ -1,11 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-
 using SpaceRace.PlayerTools;
-using SpaceRace;
 
 namespace SpaceRace.World.Buildings
 {
