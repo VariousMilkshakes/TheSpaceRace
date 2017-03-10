261522ed43079f5462907a45a77c2b70c0d6ddef
Clare Buckley <bucklec2@aston.ac.uk>
2017-03-10 15:50:58 +0000

    Add university

---
+++ Unity/Assets/Scripts/World/Buildings/Collection/University.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using SpaceRace.PlayerTools;
+using UnityEngine;
+
+namespace SpaceRace.World.Buildings.Collection
+{
+    class University : Building
+    {
+        public const string BUILDING_NAME = "University";
+
+        /// <summary>
+        /// Load sprites as singletons associated with worldstates
+        /// Means that a sprite is only loaded once
+        /// </summary>
+	    private static Dictionary<WorldStates, Sprite> loaded_sprites = new Dictionary<WorldStates, Sprite>();
+
+        public University(Player builder, Tile pos)
+            : base(typeof(University), builder, pos, loaded_sprites) { }
+
+        public override Sprite GetActiveSprite()
+        {
+            return loaded_sprites[_buildingState];
+        }
+
+        /// <summary>
+        /// The resources required to construct the building
+        /// </summary>
+        /// <returns>ResourceBox required for this building</returns>
+        public override ResourceBox BuildRequirements()
+        {
+            return GameRules.CONFIG_REPO[CONFIG].GetPropertyResourceBox(BUILDING_NAME, BUILDING_REQUIREMENTS, true);
+        }
+    }
+}
