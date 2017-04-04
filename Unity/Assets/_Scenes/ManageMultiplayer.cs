c7bd642500366265d4c4b78026597505f8fd43e0
VariousMilkshakes <vm@hadesoft.io>
2017-04-04 14:12:40 +0100

    Deepa UI Scenes

---
+++ Unity/Assets/_Scenes/ManageMultiplayer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ManageMultiplayer : MonoBehaviour {
+    public InputField numberOfPlayerInputField;
+    public InputField userIdInputField;
+    public Button closeButton;
+
+
+   // public MapGenerator mapGenerator;
+    public GameOptions gameOptions;
+
+
+    void OnEnable()
+    {
+        gameOptions = new GameOptions();
+
+
+        numberOfPlayerInputField.onValueChanged.AddListener(delegate { OnNumberOfPlayerChange(); });
+        userIdInputField.onValueChanged.AddListener(delegate { OnUserIdChange(); });
+        closeButton.onClick.AddListener(delegate { OnCloseClickEvent(); });
+    } 
+    public void OnNumberOfPlayerChange() //allows user to input number of player
+                                         //they want to play with
+    {
+
+    }
+    public void OnUserIdChange() //allows users to input the player ID 
+    {
+
+    }
+    public void OnCloseClickEvent() //takes player back to option menu upon clicking the button. 
+    {
+         
+    }
+    
+
+
+}
---
+++ Unity/Assets/_Scenes/ManageOptionMenu.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Scene1_OptionMenu : MonoBehaviour {
+
+    
+    public Button worldSeedButton;
+    public Slider waterCoverageSlider;
+    public Button playerSettingButton;
+    public Button backButton;
+    
+    
+
+
+        //public Resolution resolutions;
+        public MapGenerator mapGenerator;
+        public Scene1_OptionMenu optionMenu;
+
+
+        void OnEnable()
+        {
+            optionMenu = new Scene1_OptionMenu();
+
+
+
+
+            worldSeedButton.onClick.AddListener(delegate { OnWorldSeedClickEvent(); });
+            waterCoverageSlider.onValueChanged.AddListener(delegate { OnWaterCoverageChange(); });
+            playerSettingButton.onClick.AddListener(delegate { OnPlayerSettingClickEvent(); });
+            backButton.onClick.AddListener(delegate { OnBackClickEvent(); });
+
+
+
+            //  mapGenerator = Screen.;
+
+        }
+
+        //GameOptions.SetMapHeight(int height) { 
+        //  MapGenerator.SetMapWidth (String 
+
+
+
+        
+        public void OnWorldSeedClickEvent() //allows the user to enter the world seed
+        {
+
+        }
+        public void OnWaterCoverageChange() //user able to input the water level depending on the coverage they want. 
+        {
+
+        }
+        public void OnPlayerSettingClickEvent()//click on event which will bring out multiplayer menu upon clicking the button
+        {
+
+        }
+        public void OnBackClickEvent() //user is taken to the game
+        {
+          
+        
+        }
+    }
+
